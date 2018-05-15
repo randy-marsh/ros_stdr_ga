544ec7bea24c932711d4b18ecde7b125a4f3c19c
Juan <juanignacio.fherran@gmail.com>
2018-05-15 20:55:16 +0200

    added pybrain network

--- scripts/evolution.py
+++ scripts/evolution.py
@@ -4,3 +4,8 @@ Created on Tue May 15 20:07:37 2018
 Evolution.py
 @author: Juan
 """
+
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure.modules.sigmoidlayer import SigmoidLayer
+
+ann = buildNetwork(4,1,2, hiddenclass=SigmoidLayer)
