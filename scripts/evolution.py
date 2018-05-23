d4bea9d37180ae702ce34849ae7d32f8f1651608
Juan <juanignacio.fherran@gmail.com>
2018-05-23 19:51:10 +0200

    changed sigmoid to tanh

--- scripts/evolution.py
+++ scripts/evolution.py
@@ -56,10 +56,10 @@ if __name__ == '__main__':
     
     final_pop = ga.evolve(generator=generate_phenotype,
                           evaluator=evaluate_population,
-                          pop_size=100,
+                          pop_size=10,
 #                          num_selected=0,
                           maximize=True,
-                          max_evaluations=1000,
+                          max_evaluations=100,
                           crossover_rate=0.9,
                           mutation_rate=0.2,
                           num_inputs=9,
